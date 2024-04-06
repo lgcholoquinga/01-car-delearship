bd6762f1f6fd3528d0d73f3b8081bc4d11274491
lgcholoquinga <luisg.choloquinga@gamil.com>
2024-04-05 20:38:58 -0500

    brands

---
+++ src/brands/brands.service.ts
@@ -0,0 +1,55 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+import { Brand } from './entities/brand.entity';
+import { CreateBrandDto, UpdateBrandDto } from './dto';
+
+@Injectable()
+export class BrandsService {
+  private _brands: Brand[] = [
+    {
+      id: uuid(),
+      name: 'toyota',
+      createAt: new Date().getTime(),
+    },
+  ];
+
+  create(createBrandDto: CreateBrandDto) {
+    const brand: Brand = {
+      id: uuid(),
+      name: createBrandDto.name.toLocaleLowerCase(),
+      createAt: new Date().getTime(),
+    };
+
+    this._brands.push(brand);
+    return 'Brand created successfully.';
+  }
+
+  findAll() {
+    return this._brands;
+  }
+
+  findOne(id: string) {
+    const brand = this._brands.find((brand) => brand.id === id);
+    if (!brand) throw new NotFoundException(`Brand with id: ${id} not found.`);
+    return brand;
+  }
+
+  update(id: string, updateBrandDto: UpdateBrandDto) {
+    let brandDB = this.findOne(id);
+    this._brands = this._brands.map((brand) => {
+      if (brand.id === id) {
+        brandDB.updatedAt = new Date().getTime();
+        brandDB = { ...brandDB, ...updateBrandDto };
+        return brandDB;
+      }
+      return brand;
+    });
+    return { existError: false, message: `Brand with id: ${id} updated.` };
+  }
+
+  remove(id: string) {
+    this.findOne(id);
+    this._brands = this._brands.filter((x) => x.id !== id);
+    return { existError: false, message: `Brand with id ${id} deleted.` };
+  }
+}
